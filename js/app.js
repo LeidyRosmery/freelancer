5377d39b0dd7146fc1495f72dc371880f176e85a
LeidyRosmery <rosm3rita@hotmail.com>
2017-05-10 14:01:53 -0500

    terminado

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
-/*
- * Archivo principal de JS
- */
+
+
+var encabezado=document.getElementsByClassName("header");
+
+var lastScrollTop = 0;
+window.addEventListener("scroll", function() {
+    var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+    if (currentScroll > lastScrollTop) {
+encabezado[0].style.height= "70px";
+    }
+    else{
+      if( currentScroll <= 3 ){
+        encabezado[0].style.height= "100px";
+      }
+    }
+    lastScrollTop = currentScroll;
+}, false);
